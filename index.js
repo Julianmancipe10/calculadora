b1afcefcd29d01b61dbdce3b778a273e1f6de223
Julian Mancipe <manciperuizcenteno@gmail.com>
2024-02-25 19:14:24 -0500

    Se agrega una nueva funcion llamada restar en la cual hace la operacion para restar dos numeros

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//funcion para restar dos numeros 
+function restar(num1,num2) {
+  return num1 - num2;
+}
+
+
+
 // Función para sumar dos números
 function sumar(num1, num2) {
   return num1 + num2;
